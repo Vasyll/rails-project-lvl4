6744cfad417154ee445c9e8d2d979192254e498c
Vyacheslav Ryzhov <chjusp@gmail.com>
2022-06-29 19:56:47 +0300

    Add checks_controller

---
+++ app/controllers/api/checks_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Api::ChecksController < Web::Repositories::ApplicationController
+  def create
+ end
+
+  def show
+  end
+end
+
---
+++ app/controllers/web/repositories/checks_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Web::Repositories::ChecksController < Web::Repositories::ApplicationController
+  def create
+ end
+
+  def show
+  end
+end
+
